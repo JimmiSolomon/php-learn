89bff9005557ee399940b9770873165aa05b2cfd
Artem Sky <ok123x@yandex.ru>
2023-07-31 11:49:36 +0300

    add massive.php

---
+++ massive.php
@@ -0,0 +1,10 @@
+<?php
+$city = ['name => 'SuperCid'];
+
+$city['streets'] = [
+	['name' => 'Mira', 'buldings_count' => 5, 'old' => 3],
+	['name' => 'Mikak', 'buldings_count' => 2, 'old' => 5],
+	['name' => 'Pushkina', 'buldings_count' => 3, 'old' => [1,2,3,4]]
+
+
+	];
